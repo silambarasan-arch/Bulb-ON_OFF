fe908862347008ee5d836c7c1cb89037496459e7
silambarasan-arch <silambarasanmayakannan@gmail.com>
2025-06-26 15:29:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//https://cdn.pixabay.com/photo/2014/04/03/10/32/bulb-310821_640.png
+//https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_640.png
+
+
+const  on_btn = document.getElementById("on");
+const  off_btn = document.getElementById("off");
+var  img =document.getElementById("bulb");
+
+function bulb_on()
+{
+ img.src="https://cdn.pixabay.com/photo/2014/04/03/10/32/bulb-310821_640.png";
+
+}
+
+function bulb_off()
+{
+img.src="https://cdn.pixabay.com/photo/2014/04/03/10/42/bulb-311155_640.png";
+}
